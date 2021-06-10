e0269441f1022559f362ae5de8d1f47bdc2c721d
Rob J. Calderon <rob.jcalderon@gmail.com>
2021-06-09 20:41:13 -0700

    betting bots v1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
-import dice from '../logos/dice.webp';
-import eth from '../logos/eth.png';
-import './App.css';
+import React, { Component } from "react";
+import dice from "../logos/dice.webp";
+import eth from "../logos/eth.png";
+import "./App.css";
 
 class Main extends Component {
-
   render() {
     return (
-      <div className="container-fluid mt-5 col-m-4" style={{ maxWidth: '550px' }}>
+      <div
+        className="container-fluid mt-5 col-m-4"
+        style={{ maxWidth: "550px" }}
+      >
         <div className="col-sm">
-          <main role="main" className="col-lg-12 text-monospace text-center text-white">
+          <main
+            role="main"
+            className="col-lg-12 text-monospace text-center text-white"
+          >
             <div className="content mr-auto ml-auto">
-              <div id="content" className="mt-3" >
+              <div id="content" className="mt-3">
                 <div className="card mb-4 bg-dark border-danger">
                   <div className="card-body">
                     <div>
                       <a
-                        href="http://www.dappuniversity.com/bootcamp"
+                        href="http://botsdapp.com"
                         target="_blank"
                         rel="noopener noreferrer"
                       >
@@ -36,7 +41,8 @@ class Main extends Component {
                       />
                       <div className="input-group-append">
                         <div className="input-group-text">
-                          <img src={eth} height="20" alt=""/>&nbsp;<b>ETH</b>
+                          <img src={eth} height="20" alt="" />
+                          &nbsp;<b>ETH</b>
                         </div>
                       </div>
                     </div>
@@ -44,64 +50,104 @@ class Main extends Component {
                       type="submit"
                       className="btn btn-danger btn-lg"
                       onClick={(event) => {
-                        event.preventDefault()
+                        event.preventDefault();
                         //start with digit, digit+dot* or single dot*, end with digit.
-                        var reg = new RegExp("^[0-9]*.?[0-9]+$")    
+                        var reg = new RegExp("^[0-9]*.?[0-9]+$");
 
-                        if(reg.test(this.props.amount)){
-                          const amount = (this.props.amount).toString()
-                          this.props.makeBet(0, this.props.web3.utils.toWei(amount))
+                        if (reg.test(this.props.amount)) {
+                          const amount = this.props.amount.toString();
+                          this.props.makeBet(
+                            0,
+                            this.props.web3.utils.toWei(amount)
+                          );
                         } else {
-                          window.alert('Please type positive interger or float numbers')
+                          window.alert(
+                            "Please type positive interger or float numbers"
+                          );
                         }
-                      }}>
-                        Low
+                      }}
+                    >
+                      Low
                     </button>
                     &nbsp;&nbsp;&nbsp;
                     <button
                       type="submit"
                       className="btn btn-success btn-lg"
                       onClick={(event) => {
-                        event.preventDefault()
+                        event.preventDefault();
                         //start with digit, digit+dot* or single dot*, end with digit.
-                        var reg = new RegExp("^[0-9]*.?[0-9]+$")
-                        var minBet = Number(this.props.web3.utils.fromWei((this.props.minBet).toString())).toFixed(5)
-                        
-                        if(reg.test(this.props.amount) && this.props.amount>=minBet){
-                          const amount = (this.props.amount).toString()
-                          this.props.makeBet(1, this.props.web3.utils.toWei(amount))
+                        var reg = new RegExp("^[0-9]*.?[0-9]+$");
+                        var minBet = Number(
+                          this.props.web3.utils.fromWei(
+                            this.props.minBet.toString()
+                          )
+                        ).toFixed(5);
+
+                        if (
+                          reg.test(this.props.amount) &&
+                          this.props.amount >= minBet
+                        ) {
+                          const amount = this.props.amount.toString();
+                          this.props.makeBet(
+                            1,
+                            this.props.web3.utils.toWei(amount)
+                          );
                         } else {
-                          window.alert('Please make sure that:\n*You typed positive interger or float number\n* Typed value is >= than MinBet (not all ETH decimals visible)\n* You are using Rinkeby network')
+                          window.alert(
+                            "Please make sure that:\n*You typed positive interger or float number\n* Typed value is >= than MinBet (not all ETH decimals visible)\n* You are using Rinkeby network"
+                          );
                         }
-                      }}>
-                        High
+                      }}
+                    >
+                      High
                     </button>
                   </div>
                   <div>
-                    {!this.props.balance ? <div id="loader" className="spinner-border float-right" role="status"></div> :
-                      <div className="float-right" style={{ width: '220px' }}>
-                        <div className="float-left" style={{ height: '17px' }}>
+                    {!this.props.balance ? (
+                      <div
+                        id="loader"
+                        className="spinner-border float-right"
+                        role="status"
+                      ></div>
+                    ) : (
+                      <div className="float-right" style={{ width: "220px" }}>
+                        <div className="float-left" style={{ height: "17px" }}>
                           <b>MaxBet&nbsp;</b>
                         </div>
-                        <div className="float-right" style={{ height: '17px' }}>
-                          {Number(this.props.web3.utils.fromWei((this.props.maxBet).toString())).toFixed(5)} <b>ETH&nbsp;</b>
-                        </div>                      
+                        <div className="float-right" style={{ height: "17px" }}>
+                          {Number(
+                            this.props.web3.utils.fromWei(
+                              this.props.maxBet.toString()
+                            )
+                          ).toFixed(5)}{" "}
+                          <b>ETH&nbsp;</b>
+                        </div>
                         <br></br>
-                        <div className="float-left" style={{ height: '17px' }}>
+                        <div className="float-left" style={{ height: "17px" }}>
                           <b>MinBet</b>($1)&nbsp;
                         </div>
-                        <div className="float-right" style={{ height: '17px' }}>
-                          {Number(this.props.web3.utils.fromWei((this.props.minBet).toString())).toFixed(5)} <b>ETH&nbsp;</b>
+                        <div className="float-right" style={{ height: "17px" }}>
+                          {Number(
+                            this.props.web3.utils.fromWei(
+                              this.props.minBet.toString()
+                            )
+                          ).toFixed(5)}{" "}
+                          <b>ETH&nbsp;</b>
                         </div>
                         <br></br>
                         <div className="float-left">
                           <b>Balance&nbsp;</b>
                         </div>
                         <div className="float-right">
-                          {Number(this.props.web3.utils.fromWei((this.props.balance).toString())).toFixed(5)} <b>ETH&nbsp;</b>
+                          {Number(
+                            this.props.web3.utils.fromWei(
+                              this.props.balance.toString()
+                            )
+                          ).toFixed(5)}{" "}
+                          <b>ETH&nbsp;</b>
                         </div>
                       </div>
-                    }
+                    )}
                   </div>
                 </div>
               </div>
